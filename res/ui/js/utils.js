ffd78de656123d0513bcb015d38fce22b5be7621
mohsen_s <mohsen_s>
2006-03-23 19:39:55 +0000

    *** empty log message ***

---
+++ res/ui/js/utils.js
@@ -0,0 +1,90 @@
+/*
+ *               In the name of Allah
+ * This file is part of The Zekr Project. Use is subject to
+ * license terms.
+ *
+ * version 1
+ */
+
+// initializations...
+// window.status = '';
+
+var oldAyaId = null;
+
+function focusOnAya(ayaId) {
+	var aya = document.getElementById(ayaId);
+	if (!aya) return;
+	if (oldAyaId != null)
+		unHighlightAya(oldAyaId);
+	highlightAya(ayaId);
+	if (getBrowserHeight() > getObjectHeight(aya))
+		window.scrollTo(getX(aya), getY(aya) - getBrowserHeight()/6);
+	else
+		window.scrollTo(getX(aya), getY(aya));
+	oldAyaId = ayaId;
+}
+
+function getObjectHeight(obj) {
+	return obj.offsetHeight;
+}
+
+function getBrowserHeight() {
+	return document.body.clientHeight;
+}
+
+function highlightAya(id) {
+	if (elem = document.getElementById('sign_' + id)) {
+		elem.className = 'selectedAyaSign';
+		document.getElementById(id).className = 'selectedAya';
+	}
+}
+
+function unHighlightAya(id) {
+	if (elem = document.getElementById('sign_' + id)) {
+		elem.className = 'sign';
+		document.getElementById(id).className = 'aya';
+	}
+}
+
+function getX(elem)
+{
+	var curleft = 0;
+	if (elem.offsetParent)
+		while (1) {
+			curleft += elem.offsetLeft;
+			if (!elem.offsetParent)
+				break;
+			elem = elem.offsetParent;
+		}
+	else if (elem.x)
+		curleft += elem.x;
+	return curleft;
+}
+
+function getY(elem)
+{
+	var curtop = 0;
+	if (elem.offsetParent)
+		while (1) {
+			curtop += elem.offsetTop;
+			if (!elem.offsetParent)
+				break;
+			elem = elem.offsetParent;
+		}
+	else if (elem.y)
+		curtop += elem.y;
+	return curtop;
+}
+
+// Browser->SWT communication
+function gotoView(location) {
+	setMessage('ZEKR::GOTO ' + location + ';');
+}
+
+function translate(location) {
+	setMessage('ZEKR::TRANS ' + location + ';');
+}
+
+function setMessage(msg) {
+	window.status = msg;
+}
