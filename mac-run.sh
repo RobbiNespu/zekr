2263a01acb42ca8c83bc1c8982267180f2b6c508
mohsen_s <mohsen_s>
2006-10-23 02:56:13 +0000

    Get ready for 0.4 release

--- mac-run.sh
+++ mac-run.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-export MAIN_CLASS=net.sf.zekr.ZekrMain
-export JRE_OPT=-Djava.library.path=lib
-export CLASS_PATH=lib/log4j-1.2.8.jar:lib/swt-mac.jar:lib/apache-commons.jar:lib/velocity-1.4.jar:lib/velocity-tools-generic-1.2.jar:dist/zekr.jar
+DIR_NAME=`dirname $0`
+PATH=DIR_NAME:$PATH
+MAIN_CLASS=net.sf.zekr.ZekrMain
+JRE_OPT=-Djava.library.path=lib
+CLASS_PATH=lib/log4j-1.2.8.jar:lib/swt-mac.jar:lib/apache-commons.jar:lib/velocity-1.4.jar:lib/velocity-tools-generic-1.2.jar:dist/zekr.jar
 
-echo Launching Zekr...
+cd $DIR_NAME
 java -XstartOnFirstThread -cp "$CLASS_PATH" $JRE_OPT $MAIN_CLASS
+echo Press Command-Q to exit
