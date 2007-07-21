9fae7a0acbb31fc44beb9837b505bf76e2a51202
mohsen_s <mohsen_s>
2007-07-21 20:01:26 +0000

    Zekr 0.6.0 beta 2

--- run.bat
+++ run.bat
@@ -9,7 +9,7 @@ if JAVA_HOME=="" (
 
 :continue
 set MAIN_CLASS=net.sf.zekr.ZekrMain
-set CLASS_PATH=lib\log4j-1.2.8.jar;lib\swt-win32.jar;lib\apache-commons.jar;lib\velocity-1.4.jar;lucene-highlighter-2.1.0.jar;lucene-core-2.1.1-dev.jar;dist\zekr.jar;
+set CLASS_PATH=lib\log4j-1.2.8.jar;lib\swt-3.3.0.jar;lib\apache-commons.jar;lib\velocity-1.4.jar;lucene-highlighter-2.1.0.jar;lucene-core-2.1.1-dev.jar;dist\zekr.jar;
 set VM_ARGS=-Xms10m -Xmx70m
 echo Launching Zekr...
 :run
