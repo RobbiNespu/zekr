e02025eff5c7d3a518a0aa7c6bd785329b473f8c
mohsen_s <mohsen_s>
2007-08-17 10:08:53 +0000

    Zekr 0.6.0 final

--- linux_zekr.sh
+++ linux_zekr.sh
@@ -21,8 +21,7 @@ MAIN_CLASS=net.sf.zekr.ZekrMain
 JRE_OPT=-Djava.library.path=lib
 CLASS_PATH=lib/log4j-1.2.8.jar:lib/swt-3.3.0.jar:lib/apache-commons.jar:lib/velocity-1.4.jar:lib/lucene-highlighter-2.1.0.jar:lib/lucene-core-2.1.1-dev.jar:dist/zekr.jar
 VM_ARGS="-Xms10m -Xmx70m"
-APP_ARGS=$*
 
 cd $DIR_NAME
-"$JAVA_CMD" $VM_ARGS -cp "$CLASS_PATH" $JRE_OPT $MAIN_CLASS $APP_ARGS
+"$JAVA_CMD" $VM_ARGS -cp "$CLASS_PATH" $JRE_OPT $MAIN_CLASS $APP_ARGS $*
 cd $ORIG_DIR_NAME
--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal
-set JAVA_CMD=%JAVA_HOME%\bin\javaw.exe
+set JAVA_CMD=%JAVA_HOME%\bin\java.exe
 
 if JAVA_HOME=="" (
-	set JAVA_CMD="javaw.exe"
+	set JAVA_CMD="java.exe"
 	goto continue
 )
 
@@ -11,8 +11,7 @@ if JAVA_HOME=="" (
 set MAIN_CLASS=net.sf.zekr.ZekrMain
 set CLASS_PATH=lib\log4j-1.2.8.jar;lib\swt-3.3.0.jar;lib\apache-commons.jar;lib\velocity-1.4.jar;lucene-highlighter-2.1.0.jar;lucene-core-2.1.1-dev.jar;dist\zekr.jar;
 set VM_ARGS=-Xms10m -Xmx70m
-echo Launching Zekr...
 :run
-"%JAVA_CMD%" %VM_ARGS% -cp "%CLASS_PATH%" %MAIN_CLASS%
+"%JAVA_CMD%" %VM_ARGS% -cp "%CLASS_PATH%" %MAIN_CLASS% %*
 
 endlocal
