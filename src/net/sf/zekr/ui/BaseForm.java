5a7123f4703df2aebed281189108dd3d7dbd5948
mohsen_s <mohsen_s>
2008-02-28 20:50:42 +0000

    *** empty log message ***

--- src/net/sf/zekr/ui/BaseForm.java
+++ src/net/sf/zekr/ui/BaseForm.java
@@ -10,31 +10,31 @@ package net.sf.zekr.ui;
 
 import net.sf.zekr.common.config.ResourceManager;
 import net.sf.zekr.engine.language.LanguageEngine;
+import net.sf.zekr.engine.log.Logger;
 import net.sf.zekr.ui.helper.EventProtocol;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 public abstract class BaseForm implements EventProtocol {
-	protected LanguageEngine langEngine = LanguageEngine.getInstance();
+	final protected Logger logger = Logger.getLogger(this.getClass());
+	protected LanguageEngine lang = LanguageEngine.getInstance();
 	protected final ResourceManager resource = ResourceManager.getInstance();
+	protected Shell shell, parent;
+	protected Display display;
 
 	public void show() {
-		getShell().open();
+		shell.open();
 	}
 
-	abstract protected Shell getShell();
-
-	abstract protected Display getDisplay();
-
 	public boolean isDisposed() {
-		return getShell().isDisposed();
+		return shell.isDisposed();
 	}
 
 	public void loopEver() {
-		while (!getShell().isDisposed()) {
-			if (!getDisplay().readAndDispatch()) {
-				getDisplay().sleep();
+		while (!isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
 			}
 		}
 	}
