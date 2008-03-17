a15910ca11426cb4096802cbcc08b14508476b0c
mohsen_s <mohsen_s>
2008-03-17 21:25:44 +0000

    tanzil javascript searcher, converted to java

---
+++ src/net/sf/zekr/engine/search/tanzil/LetterConstants.java
@@ -0,0 +1,106 @@
+/*
+ *               In the name of Allah
+ * This file is part of The Zekr Project. Use is subject to
+ * license terms.
+ *
+ * Author:         Mohsen Saboorian
+ * Start Date:     Mar 17, 2008
+ */
+package net.sf.zekr.engine.search.tanzil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LetterConstants {
+	public static Map UChars = new HashMap();
+	static {
+		// letters used in Quran text
+		UChars.put("HAMZA", new Character('\u0621'));
+		UChars.put("ALEF_WITH_MADDA_ABOVE", new Character('\u0622'));
+		UChars.put("ALEF_WITH_HAMZA_ABOVE", new Character('\u0623'));
+		UChars.put("WAW_WITH_HAMZA_ABOVE", new Character('\u0624'));
+		UChars.put("ALEF_WITH_HAMZA_BELOW", new Character('\u0625'));
+		UChars.put("YEH_WITH_HAMZA", new Character('\u0626'));
+		UChars.put("ALEF", new Character('\u0627'));
+		UChars.put("BEH", new Character('\u0628'));
+		UChars.put("MARBUTA", new Character('\u0629'));
+		UChars.put("TEH", new Character('\u062A'));
+		UChars.put("THEH", new Character('\u062B'));
+		UChars.put("JEMM", new Character('\u062C'));
+		UChars.put("HAH", new Character('\u062D'));
+		UChars.put("KHAH", new Character('\u062E'));
+		UChars.put("DAL", new Character('\u062F'));
+		UChars.put("THAL", new Character('\u0630'));
+		UChars.put("REH", new Character('\u0631'));
+		UChars.put("ZAIN", new Character('\u0632'));
+		UChars.put("SEEN", new Character('\u0633'));
+		UChars.put("SHEEN", new Character('\u0634'));
+		UChars.put("SAD", new Character('\u0635'));
+		UChars.put("DAD", new Character('\u0636'));
+		UChars.put("TAH", new Character('\u0637'));
+		UChars.put("ZAH", new Character('\u0638'));
+		UChars.put("AIN", new Character('\u0639'));
+		UChars.put("GHAIN", new Character('\u063A'));
+		UChars.put("TATWEEL", new Character('\u0640'));
+		UChars.put("FEH", new Character('\u0641'));
+		UChars.put("QAF", new Character('\u0642'));
+		UChars.put("KAF", new Character('\u0643'));
+		UChars.put("LAM", new Character('\u0644'));
+		UChars.put("MEEM", new Character('\u0645'));
+		UChars.put("NOON", new Character('\u0646'));
+		UChars.put("HEH", new Character('\u0647'));
+		UChars.put("WAW", new Character('\u0648'));
+		UChars.put("ALEF_MAKSURA", new Character('\u0649'));
+		UChars.put("YEH", new Character('\u064A'));
+		UChars.put("FATHATAN", new Character('\u064B'));
+		UChars.put("DAMMATAN", new Character('\u064C'));
+		UChars.put("KASRATAN", new Character('\u064D'));
+		UChars.put("FATHA", new Character('\u064E'));
+		UChars.put("DAMMA", new Character('\u064F'));
+		UChars.put("KASRA", new Character('\u0650'));
+		UChars.put("SHADDA", new Character('\u0651'));
+		UChars.put("SUKUN", new Character('\u0652'));
+		UChars.put("MADDA", new Character('\u0653'));
+		UChars.put("HAMZA_ABOVE", new Character('\u0654'));
+		UChars.put("HAMZA_BELOW", new Character('\u0655'));
+		UChars.put("SMALL_ALEF", new Character('\u065F'));
+		UChars.put("SUPERSCRIPT_ALEF", new Character('\u0670'));
+		UChars.put("ALEF_WASLA", new Character('\u0671'));
+		UChars.put("HIGH_SALA", new Character('\u06D6'));
+		UChars.put("HIGH_GHALA", new Character('\u06D7'));
+		UChars.put("HIGH_MEEM_INITIAL_FORM", new Character('\u06D8'));
+		UChars.put("HIGH_LA", new Character('\u06D9'));
+		UChars.put("HIGH_JEMM", new Character('\u06DA'));
+		UChars.put("HIGH_THREE_DOT", new Character('\u06DB'));
+		UChars.put("HIGH_SEEN", new Character('\u06DC'));
+		UChars.put("RUB_EL_HIZB", new Character('\u06DE'));
+		UChars.put("HIGH_ROUNDED_ZERO", new Character('\u06DF'));
+		UChars.put("HIGH_UPRIGHT_ZERO", new Character('\u06E0'));
+		UChars.put("HIGH_MEEM", new Character('\u06E2'));
+		UChars.put("LOW_SEEN", new Character('\u06E3'));
+		UChars.put("SMALL_WAW", new Character('\u06E5'));
+		UChars.put("SMALL_YEH", new Character('\u06E6'));
+		UChars.put("HIGH_NOON", new Character('\u06E8'));
+		UChars.put("SAJDAH", new Character('\u06E9'));
+		UChars.put("LOW_STOP", new Character('\u06EA'));
+		UChars.put("HIGH_STOP", new Character('\u06EB'));
+		UChars.put("HIGH_STOP_FILLED", new Character('\u06EC'));
+		UChars.put("LOW_MEEM", new Character('\u06ED'));
+
+		// other letters
+		UChars.put("FARSI_YEH", new Character('\u06CC'));
+		UChars.put("FARSI_KEHEH", new Character('\u06A9'));
+		UChars.put("SWASH_KAF", new Character('\u06AA'));
+		UChars.put("YEH_BARREE", new Character('\u06D2'));
+	}
+
+	// letter groups
+	public static Map UGroups = new HashMap();
+	static {
+		UGroups.put("LETTER", "[%HAMZA-%YEH]");
+		UGroups.put("HARAKA", "[%FATHATAN-%MADDA%SUPERSCRIPT_ALEF]");
+		UGroups.put("SPACE", "[\\s%HIGH_SALA-%LOW_MEEM]*\\s");
+		UGroups.put("HAMZA_SHAPE", "[%HAMZA_ABOVE%HAMZA%ALEF_WITH_HAMZA_ABOVE-%YEH_WITH_HAMZA]");
+	}
+
+}
---
+++ src/net/sf/zekr/engine/search/tanzil/RegexSeachUtils.java
@@ -0,0 +1,145 @@
+/*
+ *               In the name of Allah
+ * This file is part of The Zekr Project. Use is subject to
+ * license terms.
+ *
+ * Author:         Mohsen Saboorian
+ * Start Date:     Mar 17, 2008
+ */
+package net.sf.zekr.engine.search.tanzil;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexSeachUtils extends LetterConstants {
+
+	// matching rulles
+	public static Map matchingRules = new HashMap();
+	static {
+		matchingRules.put("%HAMZA_SHAPE", "%HAMZA_SHAPE");
+		matchingRules.put("%ALEF_MAKSURA", "YY");
+		matchingRules.put("%ALEF",
+				"[%ALEF%ALEF_MAKSURA%ALEF_WITH_MADDA_ABOVE%ALEF_WITH_HAMZA_ABOVE%ALEF_WITH_HAMZA_BELOW%ALEF_WASLA]");
+		matchingRules.put("[%TEH%MARBUTA]", "[%TEH%MARBUTA]");
+		matchingRules.put("%HEH", "[%HEH%MARBUTA]");
+		matchingRules.put("%WAW", "[%WAW%WAW_WITH_HAMZA_ABOVE%SMALL_WAW]");
+		matchingRules.put("%YEH", "[%YEH%ALEF_MAKSURA%YEH_WITH_HAMZA%SMALL_YEH]");
+		matchingRules.put("YY", "[%ALEF_MAKSURA%YEH%ALEF]");
+		matchingRules.put(" ", "%SPACE");
+	}
+
+	// wildcards
+	public static Map wildcardRegs = new HashMap();
+	static {
+		wildcardRegs.put("\\.", "P");
+		wildcardRegs.put("\\*", "S");
+		wildcardRegs.put("[?؟]", "Q");
+	}
+
+	// wildcards
+	public static Map wildcards = new HashMap();
+	static {
+		wildcards.put("S", "(%LETTER|%HARAKA)*");
+		wildcards.put("Q", "%LETTER?");
+		wildcards.put("P", "%LETTER");
+	}
+
+	public static Map preProcess = new HashMap();
+	static {
+		preProcess.put("[%FARSI_YEH%YEH_BARREE]", "%YEH");
+		preProcess.put("[%FARSI_KEHEH%SWASH_KAF]", "%KAF");
+	}
+
+	// translate a symbolic regExp
+	private static final String regTrans(String str) {
+		// StringBuffer ret = new StringBuffer();
+		Pattern regex = Pattern.compile("%([A-Z_]+)");
+		Matcher matcher = regex.matcher(str);
+
+		// int lastEnd = 0;
+		while (matcher.find()) {
+			String group = matcher.group(1);
+
+			String replacement;
+			if (UGroups.containsKey(group))
+				replacement = (String) UGroups.get(group);
+			else if (UChars.containsKey(group))
+				replacement = ((Character) UChars.get(group)).toString();
+			else
+				continue;
+			// ret.append(str.substring(lastEnd, matcher.start()));
+			// ret.append(replacement);
+			str = str.replaceAll(matcher.group(0), replacement);
+			matcher = regex.matcher(str);
+			// lastEnd = matcher.end();
+		}
+		// ret.append(str.substring(lastEnd));
+		// return ret.toString();
+		return str;
+	}
+
+	// simulate preg_replace
+	private static final String pregReplace(String fromExp, String toExp, String str) {
+		fromExp = regTrans(fromExp);
+		toExp = regTrans(toExp);
+		return str.replaceAll(fromExp, toExp);
+	}
+
+	private static final String applyRules(Map rule, String str) {
+		for (Iterator iterator = rule.entrySet().iterator(); iterator.hasNext();) {
+			Entry entry = (Entry) iterator.next();
+			str = pregReplace(entry.getKey().toString(), entry.getValue().toString(), str);
+		}
+		return str;
+	}
+
+	private static final String handleSpaces(String pattern) {
+		String prev = "";
+		if ("".equals(pattern))
+			return pattern;
+		pattern = pattern.replaceAll("\\s+", " ");
+		while (!pattern.equals(prev)) {
+			prev = pattern;
+			pattern = pattern.replaceAll("^(([^\"]*\"[^\"]*\")*)([^\"\\s]*) ", "$1$3+");
+		}
+		pattern = pattern.replaceAll("_", " ");
+		pattern = pattern.replaceAll("\\+", "");
+
+		// remove extra operators
+		pattern = pattern.replaceAll("^[+|]+", "").replaceAll("[+|!]+$", "");
+		pattern = pattern.replaceAll("\\+*([+|!])\\+*", "$1");
+
+		return pattern;
+	}
+
+	// enrich arabic search pattern
+	public static String enrichPattern(String pattern, boolean ignoreHaraka) {
+		if (ignoreHaraka)
+			pattern = pregReplace("%HARAKA", "", pattern);
+
+		pattern = regTrans(pattern); // allows using letter constants in pattern
+		pattern = handleSpaces(pattern);
+		pattern = applyRules(preProcess, pattern);
+		pattern = applyRules(wildcardRegs, pattern);
+
+		// add haraka between letters
+		pattern = pregReplace("(.)", "$1%HARAKA*", pattern);
+
+		pattern = applyRules(matchingRules, pattern);
+		pattern = applyRules(wildcards, pattern);
+
+		return pattern;
+	}
+
+	public static void main(String[] args) {
+		// System.out.println(regTrans("[$FARSI_YEH$YEH_BARREE]"));
+		// System.out.println(handleSpaces("sadsa \"asdfasdf asdf asdf\"sdf as \"sdf sdf\" "));
+		// System.out.println(regTrans("$ALEF$ALEF_MAKSURA$ALEF_WITH_MADDA_ABOVE$ALEF_WITH_HAMZA_ABOVE$ALEF_WITH_HAMZA_BELOW$ALEF_WASLA"));
+		System.out.println(enrichPattern("salam?", false));
+		// System.out.println("salam azizam".replaceAll("(.)", "'$1'"));
+	}
+}
