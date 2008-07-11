3727882e13bc3348033b0336815eccdb8e586b9e
mohsen_s <mohsen_s>
2008-07-11 06:12:28 +0000

    AbstractQuranViewTemplet => AbstractSuraViewTemplate

--- src/net/sf/zekr/engine/template/AbstractSuraViewTemplate.java
+++ src/net/sf/zekr/engine/template/AbstractSuraViewTemplate.java
@@ -6,12 +6,12 @@ import net.sf.zekr.common.resource.QuranPropertiesUtils;
 import net.sf.zekr.engine.log.Logger;
 import net.sf.zekr.engine.theme.ThemeData;
 
-public abstract class AbstractQuranViewTemplate extends BaseViewTemplate {
+public abstract class AbstractSuraViewTemplate extends BaseViewTemplate {
 	protected int suraNum;
 	protected int ayaNum;
 	protected IQuranText quran;
 
-	public AbstractQuranViewTemplate(IQuranText quran, int suraNum, int ayaNum) {
+	public AbstractSuraViewTemplate(IQuranText quran, int suraNum, int ayaNum) {
 		super();
 		this.suraNum = suraNum;
 		this.ayaNum = ayaNum;
@@ -27,6 +27,7 @@ public abstract class AbstractQuranViewTemplate extends BaseViewTemplate {
 		String retStr = null;
 		ThemeData td = config.getTheme().getCurrent();
 		try {
+			engine.put("PAGE_MODE", config.getProps().getString("view.pagingMode"));
 			engine.put("BISMILLAH", quran.getBismillah(suraNum));
 			engine.put("JUZ_LIST", QuranPropertiesUtils.getJuzInsideSura(suraNum));
 			engine.put("ALL_JUZ_LIST", QuranPropertiesUtils.getSuraJuzAsList(suraNum));
@@ -34,13 +35,9 @@ public abstract class AbstractQuranViewTemplate extends BaseViewTemplate {
 			engine.put("SURA_NUM", new Integer(suraNum)); // Note: suraNum is counted from 1
 			engine.put("AYA_NUM", new Integer(ayaNum)); // Note: ayaNum is counted from 1
 			engine.put("SURA_NAME", QuranProperties.getInstance().getSura(suraNum).getName());
-			engine.put("GO_MSG", langEngine.getMeaning("GO"));
-			engine.put("SURA_MSG", langEngine.getMeaning("SURA"));
-			engine.put("AYA_MSG", langEngine.getMeaning("AYA"));
-			engine.put("PREV", langEngine.getMeaning("PREVIOUS"));
-			engine.put("NEXT", langEngine.getMeaning("NEXT"));
 			engine.put("TITLE", langEngine.getMeaning("SURA") + ": "
 					+ QuranProperties.getInstance().getSura(suraNum).getName()); // the same as SURA_NAME
+			engine.put("AYA_VIEW", resource.getString("theme.aya"));
 			retStr = engine.getUpdated(td.getPath() + "/" + resource.getString("theme.sura"));
 		} catch (Exception e) {
 			Logger.getLogger(this.getClass()).log(e);
