61921873571bb9fe21ec3c308ebba5f22950abbc
mohsen_s <mohsen_s>
2008-07-11 19:06:33 +0000

    remove @version

--- src/net/sf/zekr/engine/template/ITransformer.java
+++ src/net/sf/zekr/engine/template/ITransformer.java
@@ -4,16 +4,13 @@ package net.sf.zekr.engine.template;
  * General transformer interface.
  * 
  * @author Mohsen Saboorian
- * @since Zekr 1.0
  */
 public interface ITransformer {
 	/**
 	 * Sets a property in the context of transformer for further use in templates.
 	 * 
-	 * @param key
-	 *           property key to be accessed by that inside template
-	 * @param value
-	 *           property value
+	 * @param key property key to be accessed by that inside template
+	 * @param value property value
 	 */
 	void setProperty(String key, Object value);
 
