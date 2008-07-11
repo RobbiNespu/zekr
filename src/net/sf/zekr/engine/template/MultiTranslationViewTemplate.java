6ec191efdaec76552171eb79305e2cf0eff6eab4
mohsen_s <mohsen_s>
2008-07-11 19:23:00 +0000

    extend AbstractPageViewTemplate

--- src/net/sf/zekr/engine/template/MultiTranslationViewTemplate.java
+++ src/net/sf/zekr/engine/template/MultiTranslationViewTemplate.java
@@ -9,42 +9,44 @@
 package net.sf.zekr.engine.template;
 
 import net.sf.zekr.common.config.ApplicationConfig;
+import net.sf.zekr.common.config.IUserView;
 import net.sf.zekr.common.resource.IQuranText;
+import net.sf.zekr.common.resource.model.Aya;
+import net.sf.zekr.common.resource.model.Page;
 import net.sf.zekr.engine.translation.TranslationData;
 
 /**
  * Template for multi-translation view layout.
  * 
  * @author Mohsen Saboorian
- * @since Zekr 1.0
  */
-public class MultiTranslationViewTemplate extends AbstractSuraViewTemplate {
+public class MultiTranslationViewTemplate extends AbstractPageViewTemplate {
 	private TranslationData transDataList[];
 
 	/**
 	 * Creates a new multi-translation view template object. This object may have a Quran text plus an array of
 	 * translation data.
 	 * 
-	 * @param quran
-	 *           the Quran text
-	 * @param transDataList
-	 *           an array of translation data
-	 * @param suraNum
-	 *           the sura number to be transformed (counted from 1)
-	 * @param ayaNum
-	 *           the selected aya number within the sura (counted from 1)
-	 * @param playlistUrl
-	 *           recitation playlist URL
+	 * @param quran the Quran text
+	 * @param transDataList an array of translation data
+	 * @param suraNum the sura number to be transformed (counted from 1)
+	 * @param ayaNum the selected aya number within the sura (counted from 1)
+	 * @param playlistUrl recitation playlist URL
 	 */
-	public MultiTranslationViewTemplate(IQuranText quran, TranslationData[] transDataList, int suraNum, int ayaNum) {
-		super(quran, suraNum, ayaNum);
+	public MultiTranslationViewTemplate(IQuranText quran, TranslationData[] transDataList, IUserView userView) {
+		super(quran, userView);
 		this.transDataList = transDataList;
 		engine.put("TEXT_LAYOUT", ApplicationConfig.LINE_BY_LINE);
 		engine.put("MIXED", "true");
 		engine.put("CUSTOM", "true");
 		engine.put("TRANS_DATA", transDataList);
 
-		String[] quranSura = quran.getSura(suraNum);
-		engine.put("AYA_LIST", quranSura);
+		Page qPage = new Page(quran, quranPage);
+		Aya[][] mixed = new Aya[qPage.getSize()][1];
+		for (int i = 0; i < mixed.length; i++) {
+			mixed[i][0] = qPage.getAya(i);
+		}
+
+		engine.put("AYA_LIST", mixed);
 	}
 }
