d7c2984c2a28ec246ae601d41be372a2f45b1075
mohsen_s <mohsen_s>
2008-07-18 19:48:20 +0000

    pass IQuranText parameter to the superclass constructor

--- src/net/sf/zekr/engine/search/lucene/AdvancedSearchResult.java
+++ src/net/sf/zekr/engine/search/lucene/AdvancedSearchResult.java
@@ -10,12 +10,14 @@ package net.sf.zekr.engine.search.lucene;
 
 import java.util.List;
 
+import net.sf.zekr.common.resource.IQuranText;
 import net.sf.zekr.engine.search.AbstractSearchResult;
 import net.sf.zekr.engine.search.SearchResultItem;
 import net.sf.zekr.engine.search.comparator.AbstractSearchResultComparator;
 
 public class AdvancedSearchResult extends AbstractSearchResult {
 	/**
+	 * @param quranText 
 	 * @param results a list of {@link SearchResultItem}s
 	 * @param clause a string representation of whole-words of all the matched text parts
 	 * @param rawQuery raw user query
@@ -23,8 +25,8 @@ public class AdvancedSearchResult extends AbstractSearchResult {
 	 * @param ayaComparator the {@link AbstractSearchResultComparator} to be used for sorting results
 	 * @param ascending 
 	 */
-	public AdvancedSearchResult(List results, String clause, String rawQuery, int totalMatch,
+	public AdvancedSearchResult(IQuranText quranText, List results, String clause, String rawQuery, int totalMatch,
 			AbstractSearchResultComparator ayaComparator, boolean ascending) {
-		super(results, clause, rawQuery, totalMatch, ayaComparator, ascending);
+		super(quranText, results, clause, rawQuery, totalMatch, ayaComparator, ascending);
 	}
 }
