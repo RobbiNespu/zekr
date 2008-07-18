d30de9608eb18f15640658034f3dcf3b6e5fa3b2
mohsen_s <mohsen_s>
2008-07-18 19:46:58 +0000

    new class: ZekrIndexReader to wrap Lucene's IndexReader class, in order to prevent direct dependency in QuranForm to Lucene IndexReader

---
+++ src/net/sf/zekr/engine/search/lucene/ZekrIndexReader.java
@@ -0,0 +1,32 @@
+/*
+ *               In the name of Allah
+ * This file is part of The Zekr Project. Use is subject to
+ * license terms.
+ *
+ * Author:         Mohsen Saboorian
+ * Start Date:     Jul 17, 2008
+ */
+package net.sf.zekr.engine.search.lucene;
+
+import net.sf.zekr.common.resource.IQuranText;
+import net.sf.zekr.engine.translation.TranslationData;
+
+import org.apache.lucene.index.IndexReader;
+
+/**
+ * This class is actually a wrapper class for Lucene {@link IndexReader}. It holds the ID for the enclosing
+ * translation, if {@link IQuranText} is a {@link TranslationData}.
+ * 
+ * @author Mohsen Saboorian
+ */
+public class ZekrIndexReader {
+	public IndexReader indexReader;
+	public IQuranText quranText;
+	public String id;
+
+	public ZekrIndexReader(IQuranText quranText, String id, IndexReader indexReader) {
+		this.indexReader = indexReader;
+		this.id = id;
+		this.quranText = quranText;
+	}
+}
