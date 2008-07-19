bd7a3819f23e58aa061cb6bab96dca0cad5fab46
mohsen_s <mohsen_s>
2008-07-19 19:48:40 +0000

    - comment unused filter parameter: SHOW_SMALL_ALEF - HIGHLIGHT_WAQF_SIGN also has the SHOW_WAQF_SIGN effect

--- src/net/sf/zekr/common/resource/filter/IQuranFilter.java
+++ src/net/sf/zekr/common/resource/filter/IQuranFilter.java
@@ -12,13 +12,16 @@ public interface IQuranFilter {
 	/** Show waqf sign if this option is set */
 	public static final int SHOW_WAQF_SIGN = 1;
 
-	/** Show small alef if this option is set */
-	public static final int SHOW_SMALL_ALEF = 2;
+	//** Show small alef if this option is set */
+	// public static final int SHOW_SMALL_ALEF = 2;
 
 	/** Will apply Uthmani text rules */
 	public static final int UTHMANI_TEXT = 4;
 
-	public static final int HIGHLIGHT_WAQF_SIGN = 8;
+	/**
+	 * This filter option already has the meaning of {@link #SHOW_WAQF_SIGN}.
+	 */
+	public static final int HIGHLIGHT_WAQF_SIGN = 8 | SHOW_WAQF_SIGN;
 
 	/**
 	 * Implementations of this method manipulates the input parameter as needed.
