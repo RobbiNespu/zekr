af6196f9fecd2765829e2fb2ed63d6730d09ab67
mohsen_s <mohsen_s>
2008-08-01 17:16:30 +0000

    IPageNavigator: initial submission

---
+++ src/net/sf/zekr/ui/IPageNavigator.java
@@ -0,0 +1,16 @@
+/*
+ *               In the name of Allah
+ * This file is part of The Zekr Project. Use is subject to
+ * license terms.
+ *
+ * Author:         Mohsen Saboorian
+ * Start Date:     Aug 1, 2008
+ */
+package net.sf.zekr.ui;
+
+/**
+ * @author Mohsen Saboorian
+ */
+public interface IPageNavigator {
+	public void gotoPage(int page);
+}
