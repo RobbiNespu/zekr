fa29874671ee8bcd8376b6aa4ada0868475f7a6e
mohsen_s <mohsen_s>
2008-08-29 17:21:03 +0000

    +abbreviate()

--- src/net/sf/zekr/common/util/StringUtils.java
+++ src/net/sf/zekr/common/util/StringUtils.java
@@ -49,5 +49,30 @@ public class StringUtils {
 		System.out.println(StringUtils.getNthWord(STR, 3, ' '));
 		System.out.println(StringUtils.getNthWord(STR, 6, ' '));
 		System.out.println(StringUtils.getNthWord(STR, 7, ' '));
+
+		System.out.println(abbreviate(STR, 12));
+		System.out.println(abbreviate(STR, 0));
+		System.out.println(abbreviate(STR, 2));
+		System.out.println(abbreviate(STR, 40));
+		System.out.println(abbreviate("a b", 30));
+	}
+
+	/**
+	 * Similar to {@link org.apache.commons.lang.StringUtils#abbreviate(String, int)} method but adds no
+	 * ellipsis to the end of the abbreviated string. This method also abbreviates to the nearest space
+	 * character (\x20), so that the result string size is smaller or equal to size parameter.<br>
+	 * This method returns an empty string if no space character found within the given size range.
+	 * 
+	 * @param str string to be abbreviated
+	 * @param size boundary to which the result size of abbreviated string is less or equal
+	 * @return abbreviated string
+	 */
+	public static String abbreviate(String str, int size) {
+		if (str.length() <= size)
+			return str;
+		int index = str.lastIndexOf(' ', size);
+		if (index <= -1)
+			return "";
+		return str.substring(0, index);
 	}
 }
