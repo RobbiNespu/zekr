7bb1d1c57293d412b75a1056263efa4928869300
mohsen_s <mohsen_s>
2009-10-08 07:31:59 +0000

    reformat

--- src/net/sf/zekr/common/resource/QuranProperties.java
+++ src/net/sf/zekr/common/resource/QuranProperties.java
@@ -39,29 +39,14 @@ public class QuranProperties implements IQuranProperties {
 		return quranPropsReader.sajdaProp;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.sf.zekr.common.util.IQuranProperties#getSura(int)
-	 */
 	public SuraProperties getSura(int suraNum) {
 		return (SuraProperties) quranPropsReader.suraProp.get(suraNum - 1);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.sf.zekr.common.util.IQuranProperties#getJuz(int)
-	 */
 	public JuzProperties getJuz(int juzNum) {
 		return (JuzProperties) quranPropsReader.juzProp.get(juzNum - 1);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.sf.zekr.common.util.IQuranProperties#getSajda(int)
-	 */
 	public SajdaProperties getSajda(int sajdaNum) {
 		return (SajdaProperties) quranPropsReader.sajdaProp.get(sajdaNum - 1);
 	}
