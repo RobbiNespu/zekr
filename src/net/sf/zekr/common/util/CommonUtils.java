17c0a18aa1ff40f7336c09ea1320f7d18dc4efca
mohsen_s <mohsen_s>
2009-10-26 20:42:46 +0000

    added compareVersions

--- src/net/sf/zekr/common/util/CommonUtils.java
+++ src/net/sf/zekr/common/util/CommonUtils.java
@@ -8,6 +8,8 @@
  */
 package net.sf.zekr.common.util;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
  * @author Mohsen Saboorian
  */
@@ -16,4 +18,26 @@ public class CommonUtils {
 		public void run() {
 		}
 	};
+
+	/**
+	 * The two parameters should be exactly of type x.y.z, in which all x and y and z are integer numbers: 0-9.
+	 * 
+	 * @param ver1
+	 * @param ver2
+	 * @return
+	 */
+	public static int compareVersions(String ver1, String ver2) {
+		String[] v1 = StringUtils.split(ver1, ".");
+		String[] v2 = StringUtils.split(ver2, ".");
+		int k = Integer.parseInt(v1[0]) - Integer.parseInt(v2[0]);
+		if (k != 0) {
+			return k;
+		}
+		k = Integer.parseInt(v1[1]) - Integer.parseInt(v2[1]);
+		if (k != 0) {
+			return k;
+		}
+		k = Integer.parseInt(v1[2]) - Integer.parseInt(v2[2]);
+		return k;
+	}
 }
