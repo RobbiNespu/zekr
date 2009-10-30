bfa4dfe323e14e644abbffce27c949364e138dc3
mohsen_s <mohsen_s>
2009-10-30 15:39:47 +0000

    refactored

--- res/ui/js/common.js
+++ res/ui/js/common.js
@@ -13,10 +13,21 @@ String.prototype.trim = function() {
 var ayaFocusHooks = [];
 
 // Browser <-> SWT communication
-function gotoSuraAya(sura, aya, page) { javaFunction('ZEKR::GOTO', sura, aya, page); } // use it when sura changed
-function redirect(sura, aya) { javaFunction('ZEKR::REDIRECT', sura, aya); } // used for search results
-function translate(sura, aya) { javaFunction('ZEKR::TRANS', sura, aya); }
-// function setMessage(msg) { window.title = msg; }
+function gotoSuraAya(sura, aya, page) { // use it when sura changed
+    if (javaFunction) {
+        javaFunction('ZEKR::GOTO', sura, aya, page);
+    }
+}
+function redirect(sura, aya) { // used for search results
+    if (javaFunction) {
+        javaFunction('ZEKR::REDIRECT', sura, aya);
+    }
+}
+function translate(sura, aya) {
+    if (javaFunction) {
+        javaFunction('ZEKR::TRANS', sura, aya);
+    }
+}
 
 function error(e) {
 	alert("An unexpected error occurred:\n" + "[" + e.name + ":" + e.number + "] " + e.message + "\n");
