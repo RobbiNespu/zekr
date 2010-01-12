8d63b642a027fe34cb7a32db583c140bff14c355
mohsen_s <mohsen_s>
2010-01-12 10:51:10 +0000

    added loadLocalizedNames

--- src/net/sf/zekr/engine/common/LocalizedResource.java
+++ src/net/sf/zekr/engine/common/LocalizedResource.java
@@ -9,10 +9,13 @@
 package net.sf.zekr.engine.common;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import net.sf.zekr.common.config.ApplicationConfig;
 import net.sf.zekr.engine.language.LanguageEngine;
+
+import org.apache.commons.configuration.PropertiesConfiguration;
 
 /**
  * This class can be extended by resources (like recitation, translation, revelations) in order to add
@@ -22,23 +25,45 @@ import net.sf.zekr.engine.language.LanguageEngine;
  * 
  * @author Mohsen Saboorian
  */
-public class LocalizedResource {
-	protected Map names = new HashMap();
+abstract public class LocalizedResource {
+	/*** A map of language ISO code to localized name of reciter in that language */
+	public Map<String, String> localizedNameMap = new HashMap<String, String>();
+	public String name;
 
-	public Map getNames() {
-		return names;
+	public Map<String, String> getLocalizedNameMap() {
+		return localizedNameMap;
 	}
 
 	public String getName(String langCode) {
-		return (String) names.get(langCode);
+		return localizedNameMap.get(langCode);
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	/**
 	 * Cautious: this method should only be called upon instantiation of {@link ApplicationConfig}.
 	 * 
-	 * @return localized resource name
+	 * @return localized resource name (from {@link #localizedNameMap}) or {@link #name} if there is nothing
+	 *         localized for this language.
 	 */
 	public String getLocalizedName() {
-		return getName(LanguageEngine.getInstance().getLocale().getLanguage());
+		String langCode = LanguageEngine.getInstance().getLocale().getLanguage();
+		return localizedNameMap.containsKey(langCode) ? getName(langCode) : getName();
+	}
+
+	@SuppressWarnings("unchecked")
+	public void loadLocalizedNames(PropertiesConfiguration pc, String namePrefix) {
+		name = pc.getString(namePrefix);
+		Iterator<String> keys = pc.getKeys(namePrefix);
+		while (keys.hasNext()) {
+			String key = keys.next();
+			if (key.equals(namePrefix)) {
+				continue;
+			}
+			String lang = key.substring(namePrefix.length() + 1);
+			localizedNameMap.put(lang, pc.getString(key));
+		}
 	}
 }
