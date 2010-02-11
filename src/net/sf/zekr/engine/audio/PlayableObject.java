4ca8387fa5df2f254fb9538a624ebca6b9755bdc
mohsen_s <mohsen_s>
2010-02-11 19:11:33 +0000

    added equals and hashCode

--- src/net/sf/zekr/engine/audio/PlayableObject.java
+++ src/net/sf/zekr/engine/audio/PlayableObject.java
@@ -12,6 +12,8 @@ import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 
+import net.sf.zekr.common.util.ExceptionsUtils;
+
 /**
  * A playable can be either a {@link URL}, a {@link File} or an {@link InputStream}.
  * 
@@ -23,14 +25,17 @@ public class PlayableObject {
 	private InputStream inputStream;
 
 	public PlayableObject(URL url) {
+		ExceptionsUtils.preventNullParameter(url, "url");
 		this.url = url;
 	}
 
 	public PlayableObject(File file) {
+		ExceptionsUtils.preventNullParameter(file, "file");
 		this.file = file;
 	}
 
 	public PlayableObject(InputStream inputStream) {
+		ExceptionsUtils.preventNullParameter(inputStream, "inputStream");
 		this.inputStream = inputStream;
 	}
 
@@ -48,6 +53,24 @@ public class PlayableObject {
 
 	@Override
 	public String toString() {
-		return String.valueOf(file != null ? file.toString() : url != null ? url : inputStream);
+		return String.valueOf(file != null ? file : url != null ? url : inputStream);
+	}
+
+	@Override
+	public int hashCode() {
+		return file != null ? file.hashCode() : url != null ? url.hashCode() : inputStream.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj instanceof PlayableObject) {
+			PlayableObject po = (PlayableObject) obj;
+			return file != null ? file.equals(po.file) : url != null ? url.equals(po.url) : inputStream
+					.equals(po.inputStream);
+		}
+		return false;
 	}
 }
