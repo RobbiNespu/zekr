8770093c42552d6d02a184397c5f738876bbee7a
mohsen_s <mohsen_s>
2010-02-11 19:10:54 +0000

    added hashCode and equal methods

--- src/net/sf/zekr/engine/audio/NamedBufferedInputStream.java
+++ src/net/sf/zekr/engine/audio/NamedBufferedInputStream.java
@@ -12,26 +12,52 @@ import java.io.BufferedInputStream;
 import java.io.InputStream;
 
 /**
+ * Two {@link NamedBufferedInputStream}s are equal if they have the same name, regardless of their underlying
+ * {@link InputStream}.
+ * 
  * @author Mohsen Saboorian
  */
 public class NamedBufferedInputStream extends BufferedInputStream {
 	String name;
 
+	/**
+	 * Creates an instance of this class. Parameter <code>name</code> cannot be <code>null</code>.
+	 * 
+	 * @param name
+	 * @param in
+	 * @param size
+	 */
 	public NamedBufferedInputStream(String name, InputStream in, int size) {
 		super(in, size);
+		if (name == null) {
+			throw new NullPointerException("Parameter name cannot be null");
+		}
 		this.name = name;
 	}
 
-	public NamedBufferedInputStream(InputStream in, int size) {
-		super(in, size);
-	}
-
-	public NamedBufferedInputStream(InputStream in) {
-		super(in);
+	public String getName() {
+		return this.name;
 	}
 
 	@Override
 	public String toString() {
 		return String.format("%s[%s]", super.toString(), name);
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj instanceof NamedBufferedInputStream) {
+			NamedBufferedInputStream nbis = (NamedBufferedInputStream) obj;
+			return name.equals(nbis.getName());
+		}
+		return false;
 	}
 }
