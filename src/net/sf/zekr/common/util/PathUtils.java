041924e14083905161db723066a70f78ea7cdca4
mohsen_s <mohsen_s>
2010-03-30 05:33:52 +0000

    added ABSOLUTE_RESOURCE

--- src/net/sf/zekr/common/util/PathUtils.java
+++ src/net/sf/zekr/common/util/PathUtils.java
@@ -24,6 +24,7 @@ public class PathUtils {
 	public static final String WORKSPACE_OR_BASE_RESOURCE = "<workspace_base>";
 	public static final String WORKSPACE_RESOURCE = "<workspace>";
 	public static final String INSTALLDIR_RESOURCE = "<installdir>";
+	public static final String ABSOLUTE_RESOURCE = "<absolute>";
 	public static final String BASE_RESOURCE = "<base>";
 
 	public static Map<String, String> pathLookup = new HashMap<String, String>();
@@ -34,13 +35,16 @@ public class PathUtils {
 
 	/**
 	 * Resolves a dynamic path denoted by either of variables {@link #WORKSPACE_RESOURCE},
-	 * {@link #BASE_RESOURCE}, {@link #WORKSPACE_OR_BASE_RESOURCE}, or {@link #INSTALLDIR_RESOURCE} in the
-	 * beginning of the parameter. If the parameter doesn't contain any of the variables, simply a
-	 * <code>new File(unresolvedPath)</code> is returned.
+	 * {@link #BASE_RESOURCE}, {@link #WORKSPACE_OR_BASE_RESOURCE}, {@link #ABSOLUTE_RESOURCE} or
+	 * {@link #INSTALLDIR_RESOURCE} in the beginning of the parameter. If the parameter doesn't contain any of
+	 * the variables, simply a <code>new File(unresolvedPath)</code> is returned.
 	 * <p/>
-	 * A slash (or backslash) character should always be followed by either of variable resources mentioned.
-	 * For example <code>&lt;base&gt;/path/to/file</code> is correct while
-	 * <code>&lt;base&gt;path/to/file</code> is incorrect.
+	 * Except for {@link #ABSOLUTE_RESOURCE}, a slash (or backslash) character should always be followed by
+	 * either of variable resources mentioned. For example <code>&lt;base&gt;/path/to/file</code> is correct
+	 * while <code>&lt;base&gt;path/to/file</code> is incorrect. Be careful that a slash after &lt;absolute&gt;
+	 * is treated as a path element. Here are two examples: <br/>
+	 * 1. <code>&lt;absolute&gt;/usr/share/zekr/res</code> <br/>
+	 * 2. <code>&lt;absolute&gt;C:/Program Files/Zekr/res</code>
 	 * 
 	 * @param unresolvedPath
 	 * @param unresolvedParentFolder this parameter will be taken as the parent folder of unresolvedPath if it
@@ -58,6 +62,8 @@ public class PathUtils {
 		if (unresolvedPath.startsWith(WORKSPACE_RESOURCE)) {
 			baseDir = pathLookup.get(WORKSPACE_RESOURCE);
 			resolvedFile = new File(baseDir, unresolvedPath.substring(WORKSPACE_RESOURCE.length() + 1));
+		} else if (unresolvedPath.startsWith(ABSOLUTE_RESOURCE)) {
+			resolvedFile = new File(unresolvedPath.substring(ABSOLUTE_RESOURCE.length()));
 		} else if (unresolvedPath.startsWith(BASE_RESOURCE)) {
 			baseDir = (String) pathLookup.get(BASE_RESOURCE);
 			resolvedFile = new File(baseDir, unresolvedPath.substring(BASE_RESOURCE.length() + 1));
