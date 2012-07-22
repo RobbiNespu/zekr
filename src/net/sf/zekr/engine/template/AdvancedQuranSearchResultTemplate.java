ca98e5255684560d2887a2f4e557e80c20416c3a
mohsen_s <mohsen_s>
2012-07-22 08:29:18 +0000

    put TDATA

--- src/net/sf/zekr/engine/template/AdvancedQuranSearchResultTemplate.java
+++ src/net/sf/zekr/engine/template/AdvancedQuranSearchResultTemplate.java
@@ -8,7 +8,9 @@
  */
 package net.sf.zekr.engine.template;
 
+import net.sf.zekr.common.resource.FilteredQuranText;
 import net.sf.zekr.common.resource.IQuranText;
+import net.sf.zekr.common.resource.filter.IQuranFilter;
 import net.sf.zekr.common.util.I18N;
 import net.sf.zekr.engine.search.SearchResultModel;
 import net.sf.zekr.engine.theme.ThemeData;
@@ -18,61 +20,66 @@ import net.sf.zekr.engine.translation.TranslationData;
  * @author Mohsen Saboorian
  */
 public class AdvancedQuranSearchResultTemplate extends BaseViewTemplate {
-	protected SearchResultModel searchResult;
-	protected I18N i18n = new I18N(langEngine.getLocale());
-	protected String keyword;
-	int pageNo;
+   protected SearchResultModel searchResult;
+   protected I18N i18n = new I18N(langEngine.getLocale());
+   protected String keyword;
+   int pageNo;
 
-	/**
-	 * @param qts QuranTextSearcher instance
-	 * @param pageNo counted from 1
-	 */
-	public AdvancedQuranSearchResultTemplate(SearchResultModel searchResult, int pageNo) {
-		this.searchResult = searchResult;
-		this.pageNo = pageNo;
+   /**
+    * @param qts QuranTextSearcher instance
+    * @param pageNo counted from 1
+    */
+   public AdvancedQuranSearchResultTemplate(SearchResultModel searchResult, int pageNo) {
+      this.searchResult = searchResult;
+      this.pageNo = pageNo;
 
-		engine.put("THISISSEARCH", Boolean.TRUE);
-		engine.put("ICON_TRANSLATE", resource.getString("icon.translate"));
-	}
+      engine.put("THISISSEARCH", Boolean.TRUE);
+      engine.put("ICON_TRANSLATE", resource.getString("icon.translate"));
+   }
 
-	public String doTransform() throws TemplateTransformationException {
-		try {
-			String ret = null;
-			engine.put("COUNT", langEngine.getDynamicMeaning("SEARCH_RESULT_COUNT", new String[] {
-					i18n.localize(String.valueOf(searchResult.getTotalMatch())),
-					i18n.localize(String.valueOf(searchResult.getResultCount())) }));
-			engine.put("AYA_LIST", searchResult.getPage(pageNo));
-			engine.put("PAGE_START_NUM", new Integer(pageNo * searchResult.getMaxResultPerPage()));
-			engine.put("PAGE_NUM_MSG", langEngine.getDynamicMeaning("SEARCH_PAGE", new String[] {
-					i18n.localize(String.valueOf(pageNo + 1)),
-					i18n.localize(String.valueOf(searchResult.getResultPageCount())) }));
-			engine.put("CLAUSE", searchResult.getClause());
-			// String k = SearchUtils.arabicSimplify(searchResult.getRawQuery());
-			// engine.put("TITLE", langEngine.getDynamicMeaning("SEARCH_RESULT_TITLE", new String[] { k }));
-			engine.put("TITLE", langEngine.getDynamicMeaning("SEARCH_RESULT_TITLE", new String[] { searchResult
-					.getRawQuery() }));
+   public String doTransform() throws TemplateTransformationException {
+      try {
+         String ret = null;
+         /*@fmt:off*/
+         engine.put("COUNT", langEngine.getDynamicMeaning("SEARCH_RESULT_COUNT",
+                              new String[] { i18n.localize(String.valueOf(searchResult.getTotalMatch())),
+                                             i18n.localize(String.valueOf(searchResult.getResultCount())) }));
+         /*@fmt:on*/
+         engine.put("AYA_LIST", searchResult.getPage(pageNo));
+         engine.put("PAGE_START_NUM", new Integer(pageNo * searchResult.getMaxResultPerPage()));
+         engine.put(
+               "PAGE_NUM_MSG",
+               langEngine.getDynamicMeaning("SEARCH_PAGE", new String[] { i18n.localize(String.valueOf(pageNo + 1)),
+                     i18n.localize(String.valueOf(searchResult.getResultPageCount())) }));
+         engine.put("CLAUSE", searchResult.getClause());
+         // String k = SearchUtils.arabicSimplify(searchResult.getRawQuery());
+         // engine.put("TITLE", langEngine.getDynamicMeaning("SEARCH_RESULT_TITLE", new String[] { k }));
+         engine.put("TITLE",
+               langEngine.getDynamicMeaning("SEARCH_RESULT_TITLE", new String[] { searchResult.getRawQuery() }));
 
-			IQuranText iqt = searchResult.getQuranText();
-			if (iqt instanceof TranslationData) {
-				TranslationData td = (TranslationData) iqt;
-				engine.put("TRANSLATE", langEngine.getMeaning("QURAN"));
-				engine.put("TRANSLATION", "true");
-				engine.put("TRANS_DIRECTION", td.direction);
-				if ("rtl".equalsIgnoreCase(td.direction)) {
-					engine.put("ICON_PLAY", resource.getString("theme.icon.playRtl"));
-				} else {
-					engine.put("ICON_PLAY", resource.getString("theme.icon.play"));
-				}
-			} else { // is Quran
-				engine.put("ICON_PLAY", resource.getString("theme.icon.playRtl"));
-				engine.put("TRANSLATE", langEngine.getMeaning("TRANSLATION"));
-			}
+         IQuranText iqt = searchResult.getQuranText();
+         if (iqt instanceof TranslationData) {
+            TranslationData td = (TranslationData) iqt;
+            engine.put("TDATA", new FilteredQuranText(IQuranText.SIMPLE_MODE, IQuranFilter.HIGHLIGHT_WAQF_SIGN));
+            engine.put("TRANSLATE", langEngine.getMeaning("QURAN"));
+            engine.put("TRANSLATION", "true");
+            engine.put("TRANS_DIRECTION", td.direction);
+            if ("rtl".equalsIgnoreCase(td.direction)) {
+               engine.put("ICON_PLAY", resource.getString("theme.icon.playRtl"));
+            } else {
+               engine.put("ICON_PLAY", resource.getString("theme.icon.play"));
+            }
+         } else { // is Quran
+            engine.put("ICON_PLAY", resource.getString("theme.icon.playRtl"));
+            engine.put("TRANSLATE", langEngine.getMeaning("TRANSLATION"));
+            engine.put("TDATA", config.getTranslation().getDefault());
+         }
 
-			ThemeData theme = config.getTheme().getCurrent();
-			ret = engine.getUpdated(theme.getPath() + "/" + resource.getString("theme.search.result"));
-			return ret;
-		} catch (Exception e) {
-			throw new TemplateTransformationException(e);
-		}
-	}
+         ThemeData theme = config.getTheme().getCurrent();
+         ret = engine.getUpdated(theme.getPath() + "/" + resource.getString("theme.search.result"));
+         return ret;
+      } catch (Exception e) {
+         throw new TemplateTransformationException(e);
+      }
+   }
 }
